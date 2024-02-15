9e67b7d1cb97917c53d368eb2fcf1295632d42d2
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2024-02-15 21:03:11 +0530

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Task from './Componets/Task';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='Title'>ToDo List</h1>
+      <Task/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componets/Task.js
@@ -0,0 +1,12 @@
+import React from'react';
+
+function Task() {
+
+  return (
+    <div className="Task">
+      <h1>Task</h1>
+    </div>
+  );
+}
+
+export default Task;
